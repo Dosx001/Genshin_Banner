e232764c4a3373b8e5bde94e1da85351bb9817f5
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-03-07 11:18:24 -0800

    created update() and save()

--- banner.py
+++ banner.py
@@ -8,5 +8,18 @@ class banner:
         self.characters = info['characters']
 
     def __repr__(self):
-        return f"{self.characters}"
+        output = f"count: {self.count}\n"
+        for char in self.characters:
+            output += f"{char}: {self.characters[char]}\n"
+        return output
 
+    def update(self, chars):
+        self.count += 1
+        for char in chars:
+            self.characters[char].append(self.count)
+
+    def save(self):
+        with open('banner.json', 'w') as file:
+            output = {'count': self.count}
+            output.update({'characters': self.characters})
+            json.dump(output, file, indent=2)
