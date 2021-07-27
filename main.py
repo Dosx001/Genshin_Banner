2b3fdcfddd64454cc79ce84d4ba32416d168f394
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-07-27 15:28:00 -0700

    banner update

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from banner import banner
 def main():
     ban = banner()
     ban.table()
-    ban.chart()
-    ban.update(['Ningguang', 'Bennett', 'Razor'])
+    ban.update(['Fischl', 'Sucrose', 'Barbara'])
     ban.table()
+    ban.update(['Rosaria', 'Bennett', 'Razor'])
+    #ban.update(['Ningguang', 'Bennett', 'Razor'])
+    ban.table()
+    ban.chart()
 
 if __name__ == "__main__":
     main()
